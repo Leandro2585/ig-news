c5277849fb8cf1a2b26be9b007c83c2c504644e3
leandro2585 <leo.real2585@gmail.com>
2022-05-22 13:13:53 -0300

    test: ensure sign in button renders correctly when user is authenticated

--- tests/application/components/sign-in-button.spec.tsx
+++ tests/application/components/sign-in-button.spec.tsx
@@ -1,7 +1,9 @@
 import { render, screen, RenderResult } from '@testing-library/react'
+import { useSession } from 'next-auth/react'
+import { mocked } from 'ts-jest/utils'
 import { SignButton } from '../../../src/application/components/sign-button'
 
-jest.mock('next-auth/react', () => ({ useSession() { return { data: null, status: 'unauthenticated' }}}))
+jest.mock('next-auth/react')
 
 const makeSUT = (): RenderResult => {
   return render(
@@ -10,14 +12,28 @@ const makeSUT = (): RenderResult => {
 }
 
 describe('Sign Button Component', () => {
+  let useSessionMocked
   beforeAll(() => {
+    useSessionMocked = mocked(useSession).mockReturnValue({ 
+      data: { 
+        user: { 
+          name: 'John Doe', 
+          email: 'john.doe@example.com'
+        },
+        expires: 'fake-expires'
+      }, 
+      status: 'authenticated' 
+    })
   })
-
-  beforeEach(() => {
+  
+  test('renders correctly when user is authenticated', () => {
     makeSUT()
+    expect(screen.getByText('John Doe')).toBeInTheDocument()
   })
 
-  test('renders correctly when user is not authenticated', () => {
+  test('renders correctly when user is not authenticated', async () => {
+    await useSessionMocked.mockReturnValueOnce({ data: null, status: 'unauthenticated' })
+    makeSUT()
     expect(screen.getByText('Sign in with GitHub')).toBeInTheDocument()
   })
 })
